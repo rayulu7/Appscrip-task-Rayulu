c73edf923d0f6978d986dcb47c5e8c69930ee96f
rayulu7 <dasaradharam109@gmail.com>
2025-12-23 11:36:59 +0530

    add react icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
-export default function Header() {
+import React from 'react';
+import { FiSearch, FiHeart, FiShoppingCart, FiUser } from 'react-icons/fi';
+import './Header.css';
+
+function Header() {
   return (
     <header className="header">
-      <div className="header-inner">
-        <div className="logo">LOGO</div>
-
-        <nav className="nav">
-          <a>SHOP</a>
-          <a>SKILLS</a>
-          <a>STORIES</a>
-          <a>ABOUT</a>
-          <a>CONTACT US</a>
-        </nav>
-
-        <div className="header-icons">
-          <span>🔍</span>
-          <span>♡</span>
-          <span>🛒</span>
-          <span>👤</span>
-          <span className="lang">ENG ⌄</span>
-        </div>
+      <div className="logo">LOGO</div>
+      <nav>
+        <ul>
+          <li><a href="#shop">SHOP</a></li>
+          <li><a href="#skills">SKILLS</a></li>
+          <li><a href="#stories">STORIES</a></li>
+          <li><a href="#about">ABOUT</a></li>
+          <li><a href="#contact">CONTACT US</a></li>
+        </ul>
+      </nav>
+      <div className="header-icons">
+        <FiSearch className="icon" />
+        <FiHeart className="icon" />
+        <FiShoppingCart className="icon" />
+        <FiUser className="icon" />
+        <span className="lang-selector">ENG ▼</span>
       </div>
     </header>
   );
 }
+
+export default Header;
